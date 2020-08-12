78465c2b1e9170f2d783f12b6a7fbeed4b6efb94
Harshvardhan <harshvardhanvipat@gmail.com>
2020-08-12 14:29:44 +1000

    Added Unsplash API and added async request to get data using axios, also added axios to the npm package manager

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,26 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "../components/SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID ROZQiyspTafzIZGXwnVY7YPSwc3wl2uwog7JKaSsMfU",
+      },
+    });
+
+    console.log(response.data.results);
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
